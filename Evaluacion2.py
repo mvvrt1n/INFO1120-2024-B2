79d9009ff3bfbef73b4324fe8deb3fb6d95003de
Daniel Prado <dprado2024@alu.uct.cl>
2024-06-07 14:37:53 -0400

    avance en la pregunta 1

--- Evaluacion2.py
+++ Evaluacion2.py
@@ -1,1 +1,20 @@
-print("Hello World")
+import numpy as np
+import matplotlib.pyplot as plt
+import sqlite3 as sql
+import pandas as pd
+
+
+data = sql.connect('db_personas.db')
+cursor = data.cursor()  
+comandoon = '''
+Select p.rut, p.nombre_completo, p.nacionalidad, s.Rol, s.Sueldo
+FROM personas p
+INNER JOIN Salarios s 
+ON p.id_rol = s.id_salarios
+'''
+cursor.execute(comandoon)
+data = cursor.fetchall()  
+columnas = [i[0] for i in cursor.description]
+df = pd.DataFrame(data, columns=columnas)
+
+print (df)
