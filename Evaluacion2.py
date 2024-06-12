ae818ecf1ab3a8df70cfd90cb21d976b5debbf52
Martín Sanhueza F. <msanhueza2024@alu.uct.cl>
2024-06-11 20:55:46 -0400

    Cambios en las funciones

--- Evaluacion2.py
+++ Evaluacion2.py
@@ -21,203 +21,247 @@ df = pd.DataFrame(data, columns=columnas)
 
 print (df)
 
-#Definicion del la funcion filtro
-def filtro(df: pd.DataFrame):
-    graficos_barra = input("¿Le gustaria que le mostremos un grafico de los sueldos en base a las profesiones? (si/no): ").strip().lower()
+def multiples_contratos(df: pd.DataFrame, inicio:int, final:int):
     
-    if graficos_barra == 'si':
-        #Calcular el sueldo promedio por profesion
-        promedio_sueldo_prof = df.groupby('profesion')['Sueldo'].mean().reset_index()
-
-        #Crear gráfico
-        plt.figure(figsize=(10, 6))
-        plt.bar(promedio_sueldo_prof['profesion'], promedio_sueldo_prof['Sueldo'], color='skyblue')
-        plt.xlabel('Profesion')
-        plt.ylabel('Promedio Sueldo')
-        plt.title('Promedio de Sueldo x Profesión')
-        plt.xticks(rotation=45, ha='right')
-        plt.tight_layout()
-        
-        #Mostrar el gráfico
-        plt.show()
-    else:
-        print("Ok, no hay problema.")
+    if inicio < 0 or final > len(df):
+        print("Por favor, ingrese un rango válido")
+        return
     
-    graficos_torta = input("¿Le gustaria que le mostremos un grafico de torta de la distribución de profesiones? (si/no): ").strip().lower()
-    
-    if graficos_torta == 'si':
-        #Calcular la distribucion de profesiones
-        distribucion_prof = df['profesion'].value_counts()
-        
-        #Creamos grafico de torta
-        plt.figure(figsize=(4, 8))    
-        plt.pie(distribucion_prof, labels=distribucion_prof.index, autopct='%1.1f%%', startangle=140)
-        plt.title('Distribucion de Profesiones')
-        plt.axis('equal')
-        
-        #Mostrar el grafico
-        plt.show()
-    else:
-        print("Ok, sigamos...")
-        
-    graficos_conteo = input("¿Le gustaría que le mostremos un grafico de conteo de profesionales por nacionalidad? (si/no): ").strip().lower()
+    for i in range (inicio, final):
+        persona_selc  = df.iloc[i]
+        contrato1(
+            persona_selc['fecha_ingreso'],
+            persona_selc['Rol'],
+            persona_selc['residencia'],
+            persona_selc['rut'],
+            persona_selc['nombre_completo'],
+            persona_selc['nacionalidad'],
+            persona_selc['fech_de_nacimiento'],
+            persona_selc['profesion'],
+            str(persona_selc['Sueldo'])
+        )
 
-    if graficos_conteo == 'si':
-        #Calcular el conteo de profesionales por nacionalidad
-        conteo_nacionalidad = df['nacionalidad'].value_counts()
-        
-        #Crear grafico de conteo
-        plt.figure(figsize=(10, 6))
-        plt.bar(conteo_nacionalidad.index, conteo_nacionalidad, color='lightgreen')
-        plt.xlabel('Nacionalidad')
-        plt.ylabel('Cantidad de Profesionales')
-        plt.title('Conteo de Profesionales por Nacionalidad')
-        plt.xticks(rotation=45, ha='right')
-        plt.tight_layout()
-        
-        #Mostramos Grafico
-        plt.show()
-    else:
-        print("Vale, continua con la búsqueda.")        
-        
-    click = input("¿Como desea buscar a la persona?(Nombre, rut, nacionalidad, sueldo, rol): ").strip().lower()
-    
-    if click == "nombre":
-        nombreFiltro = input("Ingrese el nombre de la persona: ").strip().lower()
-        nombreSNtilde = unidecode(nombreFiltro)
-        df['nombre_completo'] = df['nombre_completo'].apply(lambda x: unidecode(x).lower())
-        filtrodf = df[df['nombre_completo'].str.contains(nombreSNtilde, case=False)]
-        
-        if not filtrodf.empty:
-            print(f"Se han encontrado a las siguientes personas con el nombre {nombreFiltro}:")
-            print(filtrodf)
-            if len(filtrodf) > 1:
-                fila_idc = int(input("Ingrese el indice de la fila que desea seleccionar: "))
-            else:
-                fila_idc = filtrodf.index[0]
+pregunta_inicio = input("¿Hola, que te gustaria hacer? \n 1: Usar el filtro \n 2: Crear multiples contratos \n 3: Ver Graficos \n Ingrese un número según su preferencia 1, 2 o 3: ")
 
-            persona_selc = filtrodf.iloc[filtrodf.index.get_loc(fila_idc)]
-            print("Datos de la persona seleccionada: ")
-            print(persona_selc)
-
-            contratoa = input("¿Desea generar el contrato de la persona seleccionada? (si/no): ").strip().lower()
-            contraoSNtilde = unidecode(contratoa)
-
-            if contraoSNtilde == "si":
-                contrato(persona_selc)
-            else:
-                print("Gracias por usar el programa")
-        else:
-            print(f"No se han encontrado personas con el nombre {nombreFiltro}")
-    
-    elif click == "rut":
-        rutfiltro = input("Ingrese el rut de la persona: ").strip()
-        filtrodf = df[df['rut'] == rutfiltro]
+if pregunta_inicio == '1':
+    #Definicion del la funcion filtro
+    def filtro(df: pd.DataFrame):    
+        click = input("¿Como desea buscar a la persona?(Nombre, rut, nacionalidad, sueldo, rol): ").strip().lower()
         
-        if not filtrodf.empty:
-            print(f"Se ha encontrado a la persona con el rut {rutfiltro}:")
-            print(filtrodf)
-
-            fila_idc = filtrodf.index[0]
-            rut_selc = filtrodf.iloc[filtrodf.index.get_loc(fila_idc)]
-            print("Datos de la persona seleccionada: ")
-            print(rut_selc)
-
-            contratoa = input("¿Desea generar el contrato de la persona seleccionada? (si/no): ").strip().lower()
-            contraoSNtilde = unidecode(contratoa)
+        if click == "nombre":
+            nombreFiltro = input("Ingrese el nombre de la persona: ").strip().lower()
+            nombreSNtilde = unidecode(nombreFiltro)
+            df['nombre_completo'] = df['nombre_completo'].apply(lambda x: unidecode(x).lower())
+            filtrodf = df[df['nombre_completo'].str.contains(nombreSNtilde, case=False)]
             
-            if contraoSNtilde == "si":
-                contrato(rut_selc)
-            else:
-                print("Gracias por usar el programa")
+            if not filtrodf.empty:
+                print(f"Se han encontrado a las siguientes personas con el nombre {nombreFiltro}:")
+                print(filtrodf)
+                if len(filtrodf) > 1:
+                    fila_idc = int(input("Ingrese el indice de la fila que desea seleccionar: "))
+                else:
+                    fila_idc = filtrodf.index[0]
 
-        else:
-            print(f"No se han encontrado personas con el rut {rutfiltro}")
-        
-    
-    elif click == "nacionalidad":
-        nacionalidadFiltro = input("Ingrese la nacionalidad de la persona: ").strip().lower()
-        df['nacionalidad'] = df['nacionalidad'].apply(lambda x: unidecode(x).lower())
-        filtrodf = df[df['nacionalidad'] == nacionalidadFiltro]
-        
-        if not filtrodf.empty:
-            print(f"Se han encontrado a las personas con la nacionalidad {nacionalidadFiltro}:")
-            print(filtrodf)
-            if len(filtrodf) > 1:
-                fila_idc = int(input("Ingrese el indice de la fila que desea seleccionar: "))
+                persona_selc = filtrodf.iloc[filtrodf.index.get_loc(fila_idc)]
+                print("Datos de la persona seleccionada: ")
+                print(persona_selc)
+
+                contratoa = input("¿Desea generar el contrato de la persona seleccionada? (si/no): ").strip().lower()
+                contraoSNtilde = unidecode(contratoa)
+
+                if contraoSNtilde == "si":
+                    contrato(persona_selc)
+                else:
+                    print("Gracias por usar el programa")
             else:
+                print(f"No se han encontrado personas con el nombre {nombreFiltro}")
+        
+        elif click == "rut":
+            rutfiltro = input("Ingrese el rut de la persona: ").strip()
+            filtrodf = df[df['rut'] == rutfiltro]
+            
+            if not filtrodf.empty:
+                print(f"Se ha encontrado a la persona con el rut {rutfiltro}:")
+                print(filtrodf)
+
                 fila_idc = filtrodf.index[0]
+                rut_selc = filtrodf.iloc[filtrodf.index.get_loc(fila_idc)]
+                print("Datos de la persona seleccionada: ")
+                print(rut_selc)
 
-            nacionalida_selc = filtrodf.iloc[filtrodf.index.get_loc(fila_idc)]
-            print("Datos de la persona seleccionada: ")
-            print(nacionalida_selc)
+                contratoa = input("¿Desea generar el contrato de la persona seleccionada? (si/no): ").strip().lower()
+                contraoSNtilde = unidecode(contratoa)
+                
+                if contraoSNtilde == "si":
+                    contrato(rut_selc)
+                else:
+                    print("Gracias por usar el programa")
 
-            contratoa = input("¿Desea generar el contrato de la persona seleccionada? (si/no): ").strip().lower()
-            contraoSNtilde = unidecode(contratoa)
-
-            if contraoSNtilde == "si":
-                contrato(nacionalida_selc)
             else:
-                print("Gracias por usar el programa")
-        else:
-            print(f"No se han encontrado personas con la nacionalidad {nacionalidadFiltro}")
-    
-    elif click == "rol":
-        rolFiltro = input("Ingrese el rol de la persona: ").strip().lower()
-        df['Rol'] = df['Rol'].apply(lambda x: unidecode(x).lower())
-        filtrodf = df[df['Rol'] == rolFiltro]
+                print(f"No se han encontrado personas con el rut {rutfiltro}")
+            
         
-        if not filtrodf.empty:
-            print(f"Se han encontrado a las personas con el rol {rolFiltro}:")
-            print(filtrodf)
+        elif click == "nacionalidad":
+            nacionalidadFiltro = input("Ingrese la nacionalidad de la persona: ").strip().lower()
+            df['nacionalidad'] = df['nacionalidad'].apply(lambda x: unidecode(x).lower())
+            filtrodf = df[df['nacionalidad'] == nacionalidadFiltro]
+            
+            if not filtrodf.empty:
+                print(f"Se han encontrado a las personas con la nacionalidad {nacionalidadFiltro}:")
+                print(filtrodf)
+                if len(filtrodf) > 1:
+                    fila_idc = int(input("Ingrese el indice de la fila que desea seleccionar: "))
+                else:
+                    fila_idc = filtrodf.index[0]
 
-            if len(filtrodf) > 1:
-                fila_idc = int(input("Ingrese el indice de la fila que desea seleccionar: "))
+                nacionalida_selc = filtrodf.iloc[filtrodf.index.get_loc(fila_idc)]
+                print("Datos de la persona seleccionada: ")
+                print(nacionalida_selc)
+
+                contratoa = input("¿Desea generar el contrato de la persona seleccionada? (si/no): ").strip().lower()
+                contraoSNtilde = unidecode(contratoa)
+
+                if contraoSNtilde == "si":
+                    contrato(nacionalida_selc)
+                else:
+                    print("Gracias por usar el programa")
             else:
-                fila_idc = filtrodf.index[0]
-
-            rol_selc = filtrodf.iloc[filtrodf.index.get_loc(fila_idc)]
-            print("Datos de la persona seleccionada: ")
-            print(rol_selc)
-
-            contratoa = input("¿Desea generar el contrato de la persona seleccionada? (si/no): ").strip().lower()
-            contraoSNtilde = unidecode(contratoa)
-
-            if contraoSNtilde == "si":
-                contrato(rol_selc)
-            else:
-                print("Gracias por usar el programa")
-        else:
-            print(f"No se han encontrado personas con el rol {rolFiltro}")
-    
-    elif click == "sueldo":
-        sueldoFiltro = int(input("Ingrese el sueldo de la persona: "))
-        filtrodf = df[df['Sueldo'] == sueldoFiltro]
+                print(f"No se han encontrado personas con la nacionalidad {nacionalidadFiltro}")
         
-        if not filtrodf.empty:
-            print(f"Se ha encontrado a la persona con el sueldo {sueldoFiltro}:")
-            print(filtrodf)
-            if len(filtrodf) > 1:
-                fila_idc = int(input("Ingrese el indice de la fila que desea seleccionar: "))
+        elif click == "rol":
+            rolFiltro = input("Ingrese el rol de la persona: ").strip().lower()
+            df['Rol'] = df['Rol'].apply(lambda x: unidecode(x).lower())
+            filtrodf = df[df['Rol'] == rolFiltro]
+            
+            if not filtrodf.empty:
+                print(f"Se han encontrado a las personas con el rol {rolFiltro}:")
+                print(filtrodf)
+
+                if len(filtrodf) > 1:
+                    fila_idc = int(input("Ingrese el indice de la fila que desea seleccionar: "))
+                else:
+                    fila_idc = filtrodf.index[0]
+
+                rol_selc = filtrodf.iloc[filtrodf.index.get_loc(fila_idc)]
+                print("Datos de la persona seleccionada: ")
+                print(rol_selc)
+
+                contratoa = input("¿Desea generar el contrato de la persona seleccionada? (si/no): ").strip().lower()
+                contraoSNtilde = unidecode(contratoa)
+
+                if contraoSNtilde == "si":
+                    contrato(rol_selc)
+                else:
+                    print("Gracias por usar el programa")
             else:
-                fila_idc = filtrodf.index[0]
+                print(f"No se han encontrado personas con el rol {rolFiltro}")
+        
+        elif click == "sueldo":
+            sueldoFiltro = int(input("Ingrese el sueldo de la persona: "))
+            filtrodf = df[df['Sueldo'] == sueldoFiltro]
+            
+            if not filtrodf.empty:
+                print(f"Se ha encontrado a la persona con el sueldo {sueldoFiltro}:")
+                print(filtrodf)
+                if len(filtrodf) > 1:
+                    fila_idc = int(input("Ingrese el indice de la fila que desea seleccionar: "))
+                else:
+                    fila_idc = filtrodf.index[0]
 
-            sueldo_selc = filtrodf.iloc[filtrodf.index.get_loc(fila_idc)]
-            print("Datos de la persona seleccionada: ")
-            print(sueldo_selc)
+                sueldo_selc = filtrodf.iloc[filtrodf.index.get_loc(fila_idc)]
+                print("Datos de la persona seleccionada: ")
+                print(sueldo_selc)
 
-            contratoa = input("¿Desea generar el contrato de la persona seleccionada? (si/no): ").strip().lower()
-            contraoSNtilde = unidecode(contratoa)
+                contratoa = input("¿Desea generar el contrato de la persona seleccionada? (si/no): ").strip().lower()
+                contraoSNtilde = unidecode(contratoa)
 
-            if contraoSNtilde == "si":
-                contrato(sueldo_selc)
+                if contraoSNtilde == "si":
+                    contrato(sueldo_selc)
+                else:
+                    print("Gracias por usar el programa")
             else:
-                print("Gracias por usar el programa")
+                print(f"No se han encontrado personas con el sueldo {sueldoFiltro}")
         else:
-            print(f"No se han encontrado personas con el sueldo {sueldoFiltro}")
-    else:
-        print("Opción no válida")
-        filtro(df)
+            print("Opción no válida")
+            filtro(df)
+
+if pregunta_inicio == '2':
+    multiples_contratos(df, 1, 1)
+
+if pregunta_inicio == '3':
+    while True:
+        graficos_barra = input("¿Le gustaria que le mostremos un grafico de los sueldos en base a las profesiones? (si/no): ").strip().lower()
+    
+        if graficos_barra == 'si':
+            #Calcular el sueldo promedio por profesion
+            promedio_sueldo_prof = df.groupby('profesion')['Sueldo'].mean().reset_index()
+
+            #Crear gráfico
+            plt.figure(figsize=(10, 6))
+            plt.bar(promedio_sueldo_prof['profesion'], promedio_sueldo_prof['Sueldo'], color='skyblue')
+            plt.xlabel('Profesion')
+            plt.ylabel('Promedio Sueldo')
+            plt.title('Promedio de Sueldo x Profesión')
+            plt.xticks(rotation=45, ha='right')
+            plt.tight_layout()
+            
+            #Mostrar el gráfico
+            plt.show()
+            break
+        
+        elif graficos_barra == 'no':
+            print("Ok, no hay problema.")
+            break
+        
+        else:
+            print('Por favor, ingrese una respuesta válida "(si/no)"')
+        
+    while True:
+        graficos_torta = input("¿Le gustaria que le mostremos un grafico de torta de la distribución de profesiones? (si/no): ").strip().lower()
+        
+        if graficos_torta == 'si':
+            #Calcular la distribucion de profesiones
+            distribucion_prof = df['profesion'].value_counts()
+            
+            #Creamos grafico de torta
+            plt.figure(figsize=(4, 8))    
+            plt.pie(distribucion_prof, labels=distribucion_prof.index, autopct='%1.1f%%', startangle=140)
+            plt.title('Distribucion de Profesiones')
+            plt.axis('equal')
+            
+            #Mostrar el grafico
+            plt.show()
+            break
+        elif graficos_torta == 'no':
+            print('Vale no hay problema')
+            break
+        else:
+            print('Por favor, ingrese una respuesta válida "(si/no)"')
+    
+    while True:    
+        graficos_conteo = input("¿Le gustaría que le mostremos un grafico de conteo de profesionales por nacionalidad? (si/no): ").strip().lower()
+
+        if graficos_conteo == 'si':
+            #Calcular el conteo de profesionales por nacionalidad
+            conteo_nacionalidad = df['nacionalidad'].value_counts()
+            
+            #Crear grafico de conteo
+            plt.figure(figsize=(10, 6))
+            plt.bar(conteo_nacionalidad.index, conteo_nacionalidad, color='lightgreen')
+            plt.xlabel('Nacionalidad')
+            plt.ylabel('Cantidad de Profesionales')
+            plt.title('Conteo de Profesionales por Nacionalidad')
+            plt.xticks(rotation=45, ha='right')
+            plt.tight_layout()
+            
+            #Mostramos Grafico
+            plt.show()
+            break
+        elif graficos_conteo == 'no':
+            print('No hay problema, sigamos...')
+        else:
+            print('Por favor, ingrese una respuesta válida "(si/no)"')
+
 
 def contrato(perso_selc):
     fecha_ingreso = perso_selc['fecha_ingreso']
@@ -317,25 +361,4 @@ def contrato1(fecha_ingreso: str, rol: str, residencia: str, rut: str, nombre_co
 
     document.save(f"{nombre_completo}_contrato.docx")
     print(f"El contrato se ha guardado como {nombre_completo}_contrato.docx")
-print(filtro(df))
-
-
-def multiples_contratos(df: pd.DataFrame, inicio:int, final:int):
-    if inicio < 0 or final > len(df):
-        print("Por favor, ingrese un rango válido")
-        return
-    
-    for i in range (inicio, final):
-        persona_selc  = df.iloc[i]
-        contrato1(
-            persona_selc['Fecha_ingreso'],
-            persona_selc['Rol'],
-            persona_selc['residencia'],
-            persona_selc['rut'],
-            persona_selc['nombre_completo'],
-            persona_selc['nacionalidad'],
-            persona_selc['fech_de_nacimiento'],
-            persona_selc['profesion'],
-            str(persona_selc['Sueldo'])
-        )
-multiples_contratos(df, 0, 5)
+print(df())
